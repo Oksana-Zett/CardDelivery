b6638a1be61ceaabe20fe014b5ada5a092830d19
Oksana Zateryukina <zateryukinaoksana@mail.ru>
2022-03-04 20:19:46 +0300

    Initial commit

---
+++ src/test/java/ru/netology/rest/CardDeliveryTest.java
@@ -0,0 +1,40 @@
+package ru.netology.rest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CardDeliveryTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setUpAll (){
+        WebDriverManager.chromedriver().setup();
+
+    }
+
+    @BeforeEach
+    public void setUp (){
+        //driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    public void tearDown (){
+        driver.quit();
+        driver=null;
+    }
+    }
